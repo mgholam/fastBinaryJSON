444d8b460819c842c06aaeeb5b302075f7344286
Mehdi Gholam <mgholam@gmail.com>
2012-03-26 14:51:25 +0430

    v1.0 - initial release

---
+++ fastBinaryJSON/Getters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fastBinaryJSON
+{
+    internal class Getters
+    {
+        public string Name;
+        public BJSON.GenericGetter Getter;
+        public Type propertyType;
+    }
+
+    public class DatasetSchema
+    {
+        public List<string> Info { get; set; }
+        public string Name { get; set; }
+    }
+}
