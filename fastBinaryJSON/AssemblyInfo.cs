8f6bf092bb8f3e60276dc7df11be787f71a63eb8
M. Gholam <mgholam@gmail.com>
2019-11-01 10:19:19 +0330

    v1.6.1 - optimized black list checking

--- fastBinaryJSON/AssemblyInfo.cs
+++ fastBinaryJSON/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.6.0.0")] 
-[assembly: AssemblyFileVersion("1.6.0.0")] 
+[assembly: AssemblyFileVersion("1.6.1.0")]
